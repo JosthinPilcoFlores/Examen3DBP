6acd3bf2e54dc9b66294bc7f3897fddd05c10f14
Josthin <jpilcof@unsa.edu.pe>
2021-08-08 00:55:16 -0500

    Añadiendo sonidos para el cambio del Login

--- app/src/main/java/com/josthin/login/LoginActivity.java
+++ app/src/main/java/com/josthin/login/LoginActivity.java
@@ -1,17 +1,19 @@
 package com.josthin.login;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    MediaPlayer login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = MediaPlayer.create(this, R.raw.message);
         Button buttonLogin = findViewById(R.id.button1);
         buttonLogin.setOnClickListener((View v) -> goToChatsActivity());
         Button buttonSignUp = findViewById(R.id.button2);
@@ -19,10 +21,12 @@ public class LoginActivity extends AppCompatActivity {
     }
     private void goToChatsActivity(){
         Intent intent = new Intent(this, ChatsActivity.class);
+        login.start();
         startActivity(intent);
     }
     private void goToSignUpActivity(){
         Intent intent = new Intent(this, SignUpActivity.class);
+        login.start();
         startActivity(intent);
     }
 }
