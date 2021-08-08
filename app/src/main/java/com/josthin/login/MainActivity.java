943b806d099c00498f1972619e65d568def1f984
Josthin <jpilcof@unsa.edu.pe>
2021-08-07 21:16:18 -0500

    Implementacion SplashScreen

---
+++ app/src/main/java/com/josthin/login/MainActivity.java
@@ -0,0 +1,36 @@
+package com.josthin.login;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //Add Animations
+        Animation animation1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+
+        ImageView logoImageView = findViewById(R.id.LogoImageView);
+
+        logoImageView.setAnimation(animation1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+    }
+}
