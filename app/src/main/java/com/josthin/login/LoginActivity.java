b3c067f81a4443cc2807592d9c9351c2d58e7f74
Josthin <jpilcof@unsa.edu.pe>
2021-08-08 01:19:36 -0500

    Añadiendo validacion de formulario básica

--- app/src/main/java/com/josthin/login/LoginActivity.java
+++ app/src/main/java/com/josthin/login/LoginActivity.java
@@ -6,9 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     MediaPlayer login;
+    private EditText e_mail, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +21,24 @@ public class LoginActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener((View v) -> goToChatsActivity());
         Button buttonSignUp = findViewById(R.id.button2);
         buttonSignUp.setOnClickListener((View v) -> goToSignUpActivity());
+        e_mail = (EditText) findViewById(R.id.EmailAddress);
+        password = (EditText) findViewById(R.id.Password);
     }
     private void goToChatsActivity(){
+        String mail = e_mail.getText().toString();
+        String pass = password.getText().toString();
         Intent intent = new Intent(this, ChatsActivity.class);
-        login.start();
-        startActivity(intent);
+        if (mail.length() == 0){
+            Toast.makeText(this, "Debes ingresar un e_mail", Toast.LENGTH_LONG).show();
+        }
+        if (pass.length() == 0){
+            Toast.makeText(this, "Debes ingresar una constraseña", Toast.LENGTH_LONG).show();
+        }
+        if (mail.length() != 0 && pass.length() != 0){
+            Toast.makeText(this, "Registro en proceso...", Toast.LENGTH_LONG).show();
+            login.start();
+            startActivity(intent);
+        }
     }
     private void goToSignUpActivity(){
         Intent intent = new Intent(this, SignUpActivity.class);
