0d495b2a2ec808dd7d8060e6ae33a1c48a7f7744
Josthin <jpilcof@unsa.edu.pe>
2021-08-08 02:30:33 -0500

    Mejora de la validacion del formulario

--- app/src/main/java/com/josthin/login/LoginActivity.java
+++ app/src/main/java/com/josthin/login/LoginActivity.java
@@ -9,9 +9,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Pattern;
+
 public class LoginActivity extends AppCompatActivity {
     MediaPlayer login;
     private EditText e_mail, password;
+    //Expresiones Regulares para validar formularios
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    "(?=.*[0-9])" +         //1 digito
+                    "(?=.*[a-z])" +         //1 letra minúscula
+                    "(?=.*[A-Z])" +         //1 letra mayúscula
+                    "(?=.*[@#$%^&+=])" +    //1 caracter especial
+                    "(?=\\S+$)" +           //No espacios
+                    ".{6,}" +               //6 caracteres
+                    "$");
+    private static final Pattern EMAIL_ADDRESS =
+            Pattern.compile(
+                    "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                            "\\@" +
+                            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                            "(" +
+                                "\\." +
+                                "[a-zA-Z0-9][a-zA-z0-9\\-]{0,25}" +
+                            ")+"
+            );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,21 +43,19 @@ public class LoginActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener((View v) -> goToChatsActivity());
         Button buttonSignUp = findViewById(R.id.button2);
         buttonSignUp.setOnClickListener((View v) -> goToSignUpActivity());
-        e_mail = (EditText) findViewById(R.id.EmailAddress);
-        password = (EditText) findViewById(R.id.Password);
+        e_mail = findViewById(R.id.EmailAddress);
+        password = findViewById(R.id.Password);
     }
     private void goToChatsActivity(){
-        String mail = e_mail.getText().toString();
-        String pass = password.getText().toString();
         Intent intent = new Intent(this, ChatsActivity.class);
-        if (mail.length() == 0){
-            Toast.makeText(this, "Debes ingresar un e_mail", Toast.LENGTH_LONG).show();
+        if (!validateEmail()){
+            confirmInput();
         }
-        if (pass.length() == 0){
-            Toast.makeText(this, "Debes ingresar una constraseña", Toast.LENGTH_LONG).show();
+        if (!validatePassword()){
+            confirmInput();
         }
-        if (mail.length() != 0 && pass.length() != 0){
-            Toast.makeText(this, "Registro en proceso...", Toast.LENGTH_LONG).show();
+        if (validateEmail() && validatePassword()){
+            Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show();
             login.start();
             startActivity(intent);
         }
@@ -44,5 +64,40 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SignUpActivity.class);
         login.start();
         startActivity(intent);
+    }
+    private boolean validatePassword(){
+        String passwordInput = password.getText().toString().trim();
+        if (passwordInput.isEmpty()){
+            password.setError("Campo Vacío");
+            return false;
+        } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()){
+            password.setError("Contraseña invalida");
+            return false;
+        }else {
+            password.setError(null);
+            return true;
+        }
+    }
+    private boolean validateEmail(){
+        String emailImput = e_mail.getText().toString().trim();
+        if (emailImput.isEmpty()){
+            e_mail.setError("Campo Vacío");
+            return false;
+        } else if (!EMAIL_ADDRESS.matcher(emailImput).matches()){
+            e_mail.setError("Ingrese un e-mail válido");
+            return false;
+        } else {
+            e_mail.setError(null);
+            return true;
+        }
+    }
+    private void confirmInput(){
+        if (!validateEmail() | !validatePassword()){
+            return;
+        }
+        String input = "Email: " + e_mail.getText().toString();
+        input += "\n";
+        input += "Password: " + password.getText().toString();
+        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
     }
 }
