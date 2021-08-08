c52c1d581fccc40b66c84cd1642b0be3b128b684
Josthin <jpilcof@unsa.edu.pe>
2021-08-07 22:50:06 -0500

    Añadiendo sonido al SplashScreen

--- app/src/main/java/com/josthin/login/MainActivity.java
+++ app/src/main/java/com/josthin/login/MainActivity.java
@@ -1,6 +1,7 @@
 package com.josthin.login;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,27 +11,28 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer hello;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        //Add Multimedia
+        hello = MediaPlayer.create(this, R.raw.hello);
         //Add Animations
         Animation animation1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
 
         ImageView logoImageView = findViewById(R.id.LogoImageView);
-
         logoImageView.setAnimation(animation1);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            hello.start();
+        }, 2000);
+        
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
         }, 4000);
     }
 }
