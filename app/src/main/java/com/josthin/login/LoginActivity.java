df833bc51867b6e61657782f10155885af055afd
Josthin <jpilcof@unsa.edu.pe>
2021-08-08 00:50:40 -0500

    Implementando Login Activity

--- app/src/main/java/com/josthin/login/LoginActivity.java
+++ app/src/main/java/com/josthin/login/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.josthin.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button buttonLogin = findViewById(R.id.button1);
+        buttonLogin.setOnClickListener((View v) -> goToChatsActivity());
+        Button buttonSignUp = findViewById(R.id.button2);
+        buttonSignUp.setOnClickListener((View v) -> goToSignUpActivity());
+    }
+    private void goToChatsActivity(){
+        Intent intent = new Intent(this, ChatsActivity.class);
+        startActivity(intent);
+    }
+    private void goToSignUpActivity(){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
